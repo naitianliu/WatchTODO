d465406208eca239f24f357aa5f530dc2d5bc711
naitianliu <naitianliu@gmail.com>
2016-02-17 21:56:30 -0800

    initiate message view controller

--- WatchTODO/WatchTODO/MainTabBarController.swift
+++ WatchTODO/WatchTODO/MainTabBarController.swift
@@ -11,10 +11,12 @@ import UIKit
 class MainTabBarController: UITabBarController {
     
     let myTodoListStoryboard = UIStoryboard(name: "MyTodoList", bundle: nil)
+    let messageStoryboard = UIStoryboard(name: "Message", bundle: nil)
     let watchStoryboard = UIStoryboard(name: "Watch", bundle: nil)
     let settingsStoryboard = UIStoryboard(name: "Settings", bundle: nil)
     
     var myTodoListNC: MyTodoListNavigationController!
+    var messageNC: UINavigationController!
     var watchNC: UINavigationController!
     var settingsNC: UINavigationController!
 
@@ -23,9 +25,16 @@ class MainTabBarController: UITabBarController {
 
         // Do any additional setup after loading the view.
         
+        self.tabBar.tintColor = const_ThemeColor
+        
         myTodoListNC = myTodoListStoryboard.instantiateViewControllerWithIdentifier("MyTodoListNavigationController") as! MyTodoListNavigationController
         myTodoListNC.tabBarItem.title = "My Todo List"
         myTodoListNC.tabBarItem.image = UIImage(named: "tab_todo_list")
+        
+        messageNC = messageStoryboard.instantiateViewControllerWithIdentifier("MessageNavigationController") as! UINavigationController
+        messageNC.tabBarItem.title = "Message"
+        messageNC.tabBarItem.image = UIImage(named: "tab_message")
+        messageNC.tabBarItem.badgeValue = "1"
         
         watchNC = watchStoryboard.instantiateViewControllerWithIdentifier("WatchNavigationController") as! UINavigationController
         watchNC.tabBarItem.title = "Watch"
@@ -35,7 +44,7 @@ class MainTabBarController: UITabBarController {
         settingsNC.tabBarItem.title = "Settings"
         settingsNC.tabBarItem.image = UIImage(named: "tab_settings")
         
-        self.viewControllers = [myTodoListNC, watchNC, settingsNC]
+        self.viewControllers = [myTodoListNC, messageNC, watchNC, settingsNC]
         
     }
 
